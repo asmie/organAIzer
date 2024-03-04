4dc99128e6cb7397f5b1977e6eec4e5b4c646be2
Piotr Olszewski <asmie@asmie.pl>
2024-03-04 10:19:22 +0100

    feat: simple code accessing gmail mails

--- src/organAIzer/cli.py
+++ src/organAIzer/cli.py
@@ -1,2 +1,16 @@
-def cli():
-    print("Hello world!")
+import argparse
+from gmail import get_messages
+
+
+def main(list_messages):
+    if list_messages:
+        messages = get_messages()
+        for message in messages:
+            print(message)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Simple CLI that lists Gmail messages.')
+    parser.add_argument('--list', dest='list_messages', action='store_true', help='List Gmail messages')
+    args = parser.parse_args()
+    main(args.list_messages)
