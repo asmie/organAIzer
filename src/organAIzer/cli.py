5d416e0ca7ed185547c4103fd6f3758087aea42b
Piotr Olszewski <asmie@asmie.pl>
2024-03-07 21:26:27 +0100

    chore: Makefile added, requirements updated, mail/calendar access tested

--- src/organAIzer/cli.py
+++ src/organAIzer/cli.py
@@ -1,16 +1,20 @@
 import argparse
 from gmail import get_messages
+from gcalendar import get_cal_events
 
 
-def main(list_messages):
+def main(list_messages, list_events):
     if list_messages:
         messages = get_messages()
         for message in messages:
             print(message)
+    elif list_events:
+        get_cal_events()
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simple CLI that lists Gmail messages.')
     parser.add_argument('--list', dest='list_messages', action='store_true', help='List Gmail messages')
+    parser.add_argument("--cal", dest="list_events", action="store_true", help="List Google Calendar events")
     args = parser.parse_args()
-    main(args.list_messages)
+    main(args.list_messages, args.list_events)
