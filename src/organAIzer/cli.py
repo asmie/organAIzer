c7f9ef3b2d43e0e50356cdb846ff1789c489d5b7
Piotr Olszewski <asmie@asmie.pl>
2024-03-14 22:52:15 +0100

    chore: displaying providers

--- src/organAIzer/cli.py
+++ src/organAIzer/cli.py
@@ -10,11 +10,21 @@ def main(list_messages, list_events):
             print(message)
     elif list_events:
         get_cal_events()
+    else:
+        display_providers()
+
+def display_providers():
+    print("Available providers:")
+    print("1. Gmail")
+    print("2. Google Calendar")
+    print("3. Outlook")
+    print("4. Office 365")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Simple CLI that lists Gmail messages.')
     parser.add_argument('--list', dest='list_messages', action='store_true', help='List Gmail messages')
     parser.add_argument("--cal", dest="list_events", action="store_true", help="List Google Calendar events")
+    parser.add_argument("--providers", dest="providers", action="store_true", help="List available providers")
     args = parser.parse_args()
     main(args.list_messages, args.list_events)
